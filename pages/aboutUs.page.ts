4a2c2fbdf2cdffc94dfc9fc7f665916ad350dc98
unknown <tededwardlongley@icloud.com>
2025-05-14 13:19:11 +0100

    first commit

---
+++ pages/aboutUs.page.ts
@@ -0,0 +1,17 @@
+import { Locator, Page } from "playwright/test";
+
+
+export default class AboutUsPage{
+
+    page: Page;
+    ourStoryText: Locator;
+
+    constructor(page: Page){
+        this.page = page;
+        this.ourStoryText = this.page.getByText('OUR STORY')
+    }
+
+    async visit(){
+        await this.page.goto('/pages/about-us');
+    }
+}
---
+++ pages/fixtures/base.ts
@@ -0,0 +1,16 @@
+import AboutUsPage from "../aboutUs.page"
+import {test as base} from 'playwright/test'
+
+
+type commonFixtures = {
+    aboutUsPage: AboutUsPage
+}
+
+export const test = base.extend<commonFixtures>({
+    
+    aboutUsPage: async({page},use)=>{
+        const aboutUsPage = new AboutUsPage(page)
+        await use(aboutUsPage)
+    }
+
+})
